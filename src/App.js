e94078d5fde7459b0c11e5780b38bcc7502413dc
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-14 17:11:38 +0300

    dynamic-routes-usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
       {
         path: '/products',
         element: <ProductsPage />,
+      },
+      {
+        path: '/products/:productId',
+        element: <ProductDetailPage />,
       },
     ],
   },
